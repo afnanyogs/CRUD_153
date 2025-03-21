7ace94685879d6f168af49daf4b63b09cbb77736
Afnan Asilah <afnanyogs@gmail.com>
2025-03-21 17:18:54 +0700

    Jika terjadi kesalahan, akan muncul pesan MessageBox.

--- CRUD/Form1.cs
+++ CRUD/Form1.cs
@@ -50,4 +50,13 @@ namespace CRUD
                     dgvMahasiswa.AutoGenerateColumns = true;
                     dgvMahasiswa.DataSource = dt;
 
-                    
+                    ClearForm();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+       
