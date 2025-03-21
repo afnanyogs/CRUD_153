ad143f274f4f858b5efc3ff2b3c331b8a5589701
Afnan Asilah <afnanyogs@gmail.com>
2025-03-21 17:04:04 +0700

    Add project files.

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNIM = new System.Windows.Forms.MaskedTextBox();
+            this.txtNama = new System.Windows.Forms.MaskedTextBox();
+            this.txtAlamat = new System.Windows.Forms.MaskedTextBox();
+            this.txtEmail = new System.Windows.Forms.MaskedTextBox();
+            this.txtTelepon = new System.Windows.Forms.MaskedTextBox();
+            this.bttnTambah = new System.Windows.Forms.Button();
+            this.bttnHapus = new System.Windows.Forms.Button();
+            this.bttnUbah = new System.Windows.Forms.Button();
+            this.bttnRefresh = new System.Windows.Forms.Button();
+            this.dgvMahasiswa = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Alamat = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMahasiswa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 10;
+            // 
+            // txtNIM
+            // 
+            this.txtNIM.Location = new System.Drawing.Point(147, 12);
+            this.txtNIM.Name = "txtNIM";
+            this.txtNIM.Size = new System.Drawing.Size(259, 22);
+            this.txtNIM.TabIndex = 9;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(147, 58);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(259, 22);
+            this.txtNama.TabIndex = 8;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(147, 158);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(259, 22);
+            this.txtAlamat.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(147, 107);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(259, 22);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtTelepon
+            // 
+            this.txtTelepon.Location = new System.Drawing.Point(147, 199);
+            this.txtTelepon.Name = "txtTelepon";
+            this.txtTelepon.Size = new System.Drawing.Size(259, 22);
+            this.txtTelepon.TabIndex = 5;
+            // 
+            // bttnTambah
+            // 
+            this.bttnTambah.Location = new System.Drawing.Point(448, 36);
+            this.bttnTambah.Name = "bttnTambah";
+            this.bttnTambah.Size = new System.Drawing.Size(75, 23);
+            this.bttnTambah.TabIndex = 4;
+            this.bttnTambah.Text = "Tambah";
+            this.bttnTambah.Click += new System.EventHandler(this.bttnTambah_Click);
+            // 
+            // bttnHapus
+            // 
+            this.bttnHapus.Location = new System.Drawing.Point(448, 81);
+            this.bttnHapus.Name = "bttnHapus";
+            this.bttnHapus.Size = new System.Drawing.Size(75, 23);
+            this.bttnHapus.TabIndex = 3;
+            this.bttnHapus.Text = "Hapus";
+            this.bttnHapus.Click += new System.EventHandler(this.bttnHapus_Click);
+            // 
+            // bttnUbah
+            // 
+            this.bttnUbah.Location = new System.Drawing.Point(448, 125);
+            this.bttnUbah.Name = "bttnUbah";
+            this.bttnUbah.Size = new System.Drawing.Size(75, 23);
+            this.bttnUbah.TabIndex = 2;
+            this.bttnUbah.Text = "Ubah";
+            this.bttnUbah.Click += new System.EventHandler(this.bttnUbah_Click);
+            // 
+            // bttnRefresh
+            // 
+            this.bttnRefresh.Location = new System.Drawing.Point(448, 177);
+            this.bttnRefresh.Name = "bttnRefresh";
+            this.bttnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.bttnRefresh.TabIndex = 1;
+            this.bttnRefresh.Text = "Refresh";
+            this.bttnRefresh.Click += new System.EventHandler(this.bttnRefresh_Click);
+            // 
+            // dgvMahasiswa
+            // 
+            this.dgvMahasiswa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMahasiswa.Location = new System.Drawing.Point(39, 262);
+            this.dgvMahasiswa.Name = "dgvMahasiswa";
+            this.dgvMahasiswa.RowHeadersWidth = 51;
+            this.dgvMahasiswa.RowTemplate.Height = 24;
+            this.dgvMahasiswa.Size = new System.Drawing.Size(698, 150);
+            this.dgvMahasiswa.TabIndex = 0;
+            this.dgvMahasiswa.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMahasiswa_CellClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(69, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "NIM";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(63, 61);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nama";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(64, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 16);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Email";
+            // 
+            // Alamat
+            // 
+            this.Alamat.AutoSize = true;
+            this.Alamat.Location = new System.Drawing.Point(64, 161);
+            this.Alamat.Name = "Alamat";
+            this.Alamat.Size = new System.Drawing.Size(49, 16);
+            this.Alamat.TabIndex = 18;
+            this.Alamat.Text = "Alamat";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(63, 202);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 16);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Telepon";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Alamat);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgvMahasiswa);
+            this.Controls.Add(this.bttnRefresh);
+            this.Controls.Add(this.bttnUbah);
+            this.Controls.Add(this.bttnHapus);
+            this.Controls.Add(this.bttnTambah);
+            this.Controls.Add(this.txtTelepon);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNIM);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CRUD Mahasiswa";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMahasiswa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox txtNIM;
+        private System.Windows.Forms.MaskedTextBox txtNama;
+        private System.Windows.Forms.MaskedTextBox txtAlamat;
+        private System.Windows.Forms.MaskedTextBox txtEmail;
+        private System.Windows.Forms.MaskedTextBox txtTelepon;
+        private System.Windows.Forms.Button bttnTambah;
+        private System.Windows.Forms.Button bttnHapus;
+        private System.Windows.Forms.Button bttnUbah;
+        private System.Windows.Forms.Button bttnRefresh;
+        private System.Windows.Forms.DataGridView dgvMahasiswa;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label Alamat;
+        private System.Windows.Forms.Label label10;
+    }
+}
