7936469363b9b48900f127f9a41f8b0a6e7045c9
Afnan Asilah <afnanyogs@gmail.com>
2025-03-21 17:17:22 +0700

    Mengambil data dari tabel Mahasiswa di SQL Server.

--- CRUD/Form1.cs
+++ CRUD/Form1.cs
@@ -35,4 +35,16 @@ namespace CRUD
             txtNIM.Focus();
         }
 
-       
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                   
